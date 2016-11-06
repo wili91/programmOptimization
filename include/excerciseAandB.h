bb2f508d422be39f66ada7a56d42e0f9632ba990
Lisa Winter <ljb.win10@gmail.com>
2016-11-06 13:18:02 +0100

    Removed gtest code. Completet excercise A

--- include/excerciseAandB.h
+++ include/excerciseAandB.h
@@ -21,57 +21,9 @@ extern const std::size_t  	CACHE_LINE_BYTES = 64;
 //4096 Byte Cache Size
 extern const std::size_t 	L1_CACHE_SIZE_INT = 1024;
 
-template<std::size_t N, typename T>
-void genrate_array(std::array<T, N>& test_arr){
-	//generate the array
-	std::random_device rd;
-	std::mt19937 mt(rd());
-	std::uniform_real_distribution<T> dist_int( 0.0,500.0);
-
-	for(std::size_t i = 0; i< test_arr.size() ; i++)
-	{
-		test_arr[i] = dist_int(mt);
-	}
-}
 
 template<std::size_t N, typename T>
-void genrate_and_measure(T (*pFunc)(std::array<T, N>&, std::size_t), std::array<T,N> test_arr)
-{
-	std::array<int, L1_CACHE_SIZE_INT > cache_flush_arr;
-
-	//flush the Cache
-	for(std::size_t i = 0; i < cache_flush_arr.size(); i++) {
-		cache_flush_arr[i] = std::numeric_limits<int>::max();
-	}
-
-	std::size_t index = test_arr.size();
-	T output;
-	while(index > 2)
-	{
-		//flush
-		//flush the Cache
-		for(std::size_t i = 0; i < cache_flush_arr.size(); i++) {
-			cache_flush_arr[i] = std::numeric_limits<int>::max();
-		}
-
-		auto start_time = std::chrono::high_resolution_clock::now();
-		output = pFunc(test_arr, index);
-		auto end_time = std::chrono::high_resolution_clock::now();
-
-		std::cout << "Minimum: "<< output << "    Time: " <<  std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count()<<std::endl;
-
-
-		index = index/2;
-	}
-
-	//genrate_and_measure<N/2, T>();
-
-}
-
-
-
-template<std::size_t N, typename T>
-T dumb_minimum(std::array<T,N>& arr, std::size_t index)
+T plain_minimum(std::array<T,N>& arr, std::size_t index)
 {
 	T min = arr[0];
 
@@ -90,12 +42,20 @@ T minimum_index(std::array<T,N>& arr, std::size_t start_index,std::size_t end_in
 
 	std::size_t min_index = start_index;
 
-	for(std::size_t i = start_index; i< end_index; i++){
-		if(arr[min_index] > arr[i])
-		{
-			min_index = i;
+	//outer loops step is once the count of what a cache line may contain
+	std::size_t outer_step= CACHE_LINE_BYTES/sizeof(T);
+	//inner loops step has to be one
+
+	for(std::size_t i = 0; i< end_index; i+=outer_step){
+		__builtin_prefetch(&arr[i+outer_step+1]);
+		for(std::size_t j = 0; j< outer_step && (i+j) < end_index; j++){
+			if(arr[min_index] > arr[i+j])
+			{
+				min_index = i+j;
+			}
 		}
 	}
+
 	return min_index;
 }
 template<std::size_t N, typename T>
--- src/programmOptimization.cpp
+++ src/programmOptimization.cpp
@@ -17,7 +17,7 @@
 
 
 #include "../include/excerciseAandB.h"
-
+#include "../include/TimeMeasurement.h"
 
 
 int  main(int arg, char** argc) {
@@ -27,7 +27,7 @@ int  main(int arg, char** argc) {
 	genrate_array(array);
 	std::cout<<"Minimum one loop:" << std::endl;
 	std::cout<<" " << std::endl;
-	genrate_and_measure<start_size, double>(dumb_minimum, array);
+	genrate_and_measure<start_size, double>(plain_minimum, array);
 
 	std::cout<<" " << std::endl;
 	std::cout<<"Minimum two loops:" << std::endl;
@@ -40,44 +40,5 @@ int  main(int arg, char** argc) {
 	std::cout<<" " << std::endl;
 	genrate_and_measure<start_size, double>(minimum_prefetch, array);
 
-
-	::testing::InitGoogleTest(&arg, argc);
-	return RUN_ALL_TESTS();
-
-
 	return 0;
 }
-
-int doppelt(int a){
-	return a*2;
-}
-
-
-TEST(Minimum, dumb_minimum){
-	std::array<int, 6> arr ={9,7,6,6,7,1};
-	EXPECT_EQ(1, dumb_minimum(arr, 6));
-
-	std::array<int, 6> arr1 ={1,9,7,0,6,7};
-	EXPECT_EQ(0, dumb_minimum(arr1, arr1.size()));
-}
-
-
-TEST(MinimumIndex, minimum_index){
-	std::array<int, 6> arr ={9,7,6,6,7,1};
-	EXPECT_EQ(5, minimum_index(arr, 3 ,arr.size()));
-	std::cout<<"Minimum is "<< arr[minimum_index(arr, 3,arr.size())]<< std::endl;
-
-	std::array<int, 6> arr1 ={1,9,7,0,6,7};
-	EXPECT_EQ(3, minimum_index(arr1, 0,4));
-	std::cout<<"Minimum is "<< arr1[minimum_index(arr1, 0,4)]<< std::endl;
-
-	EXPECT_EQ(0, minimum_index(arr1, 0,3));
-	std::cout<<"Minimum is "<< arr1[minimum_index(arr1, 0,3)]<< std::endl;
-
-	EXPECT_EQ(3, minimum_index(arr1, 0,arr1.size()));
-	std::cout<<"Minimum is "<< arr1[minimum_index(arr1, 0,arr1.size())]<< std::endl;
-
-	EXPECT_EQ(4, minimum_index(arr1, 4,arr1.size()));
-	std::cout<<"Minimum is "<< arr1[minimum_index(arr1, 4,arr1.size())]<< std::endl;
-}
-
