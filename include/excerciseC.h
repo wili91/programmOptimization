211fae24ec5d02bfe69274d73a39788e672b8085
Lisa Winter <ljb.win10@gmail.com>
2016-11-28 11:58:47 +0100

    added Prefetch to insertion sort

--- include/excerciseC.h
+++ include/excerciseC.h
@@ -23,13 +23,22 @@ template<std::size_t N, typename T>
 void prefetch_Insertionsort(std::array<T, N>& arr)
 {
 	T sortingValue;
-	std::size_t outer_step = CACHE_LINE_BYTES / sizeof(T);
+
+	const std::size_t outer_step = CACHE_LINE_BYTES / sizeof(T);
 
 	for(std::size_t i = 1; i< arr.size(); i++){
 		sortingValue = arr[i];
 
-		for ( size_t j = i; (j-outer_step) >=0 && arr[j-1] > sortingValue; j-=outer_step)
+		size_t variable_step = CACHE_LINE_BYTES / sizeof(T);;
+		if(i < outer_step){
+			variable_step = i;
+		}
+
+		for ( size_t j = i; (j-variable_step) < arr.size() && arr[j-1] > sortingValue; j-=variable_step)
 		{
+			if (variable_step == outer_step) {
+				__builtin_prefetch(&arr[j - outer_step + 1]);
+			}
 			for ( size_t k = j; k >0 && arr[k-1] > sortingValue; k--){
 				arr[k] = arr[k-1];
 				arr[k-1]= sortingValue;
--- src/ExcerciseC_Test.cpp
+++ src/ExcerciseC_Test.cpp
@@ -29,5 +29,19 @@ TEST(InsertionSort, double){
 
 		compare++;
 	}
+
+	std::shared_ptr<std::array<double, 10>> array2(
+			new std::array<double, 10>);
+
+	genrate_array_desc(*array2);
+	prefetch_Insertionsort(*array2);
+	compare = 1.0;
+	for (std::size_t i = 0; i < array2->size(); i++) {
+
+		std::cout<<(*array2)[i]<<","<<std::endl;
+		//EXPECT_EQ(compare , (*array2)[i]);
+
+		compare++;
+	}
 }
 
