faf5c1ee10f93e8af377ae2713faacb77f3fa6b7
Lisa Winter <ljb.win10@gmail.com>
2016-12-19 14:07:30 +0100

    fixed  to many runs

--- include/excerciseC.h
+++ include/excerciseC.h
@@ -18,13 +18,14 @@ void prefetch_Insertionsort(std::array<T, N>& arr) {
 
 	const std::size_t OUTER_STEP = CACHE_LINE_BYTES / sizeof(T);
 	for (std::size_t i = 1; i < arr.size(); i++) {
-		std::size_t j = (i >= (OUTER_STEP)) ? i : (OUTER_STEP);
-		for (; arr[(i - 1)] > arr[i] && j >= (OUTER_STEP); j -= OUTER_STEP) {
-			__builtin_prefetch(&arr[j - (2 * OUTER_STEP)]);
 
-			size_t id = i;
+		std::size_t id = i;
+		for (; arr[(id - 1)] > arr[id] && id >0;) {
+			__builtin_prefetch(&arr[id - (2 * OUTER_STEP)]);
+
+
 			for (size_t k = OUTER_STEP;
-					arr[(id - 1)] > arr[id] && k >= 0 && id > 0; k--, id--) {
+					arr[(id - 1)] > arr[id] && id > 0 && k > 0; k--, id--) {
 				std::swap(arr[(id - 1)], arr[id]);
 
 			}
