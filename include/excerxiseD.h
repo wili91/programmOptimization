3e67ddd3b172aa3a61efbec386f0af8057477961
Lisa Winter <ljb.win10@gmail.com>
2016-12-19 14:07:45 +0100

    added Bottom Up Merge Sort

--- include/excerxiseD.h
+++ include/excerxiseD.h
@@ -39,40 +39,62 @@ template<std::size_t N, typename T>
 void bitonic_merge(std::array<T, N>& arr1, std::array<T, N>& help,
 		std::size_t length) {
 	std::size_t left, mid, right;
-	//steps in the size of two runs
+	std::size_t left_Runitr, right_Runitr;
+
 	for (std::size_t merge_index = 0; merge_index < arr1.size();
 			merge_index = merge_index + 2 * length) {
 
 		//define the starting index, middel index and right index of current runs
 		left = merge_index;
 		mid = std::min(arr1.size(), merge_index + length);
-		right = std::min(arr1.size(), merge_index + 2 * length);
+		right = (std::min(arr1.size(), merge_index + 2 * length))-1;
 
-		//loop over run. itr is the current index of the help Array
-		for (std::size_t itr = left; itr < right; itr++) {
-			if(itr < mid) { //sort ascending
+		//copy right side
+		for (std::size_t itr = left; itr < mid; itr++) {
+			help[itr] = arr1[itr];
 
-			} else {	// sort descending
-
-			}
 		}
-	}
-}
 
+		//copy left side
+		right_Runitr = right;
+		for (std::size_t itr = mid; right_Runitr >= mid; itr++) {
+			help[itr] = arr1[right_Runitr--];
+		}
+
+		left_Runitr = left;
+		right_Runitr = right;
+
+		size_t i = merge_index;
+		while (left_Runitr <= right_Runitr) {
+
+			if (help[left_Runitr] <= help[right_Runitr]) {
+				arr1[i]=help[left_Runitr];
+				left_Runitr++;
+			} else {
+				arr1[i]=help[right_Runitr];
+				right_Runitr--;
+			}
+			i++;
+		}
+
+	}
+
+
+
+}
 
 template<std::size_t N, typename T>
 void bottomUpMergeSort(std::array<T, N>& arr) {
 
-//help array
+	//help array
 	std::shared_ptr<std::array<T, N> > help_array(new std::array<T, N>);
 
-	for (std::size_t length = 1; length < arr.size(); length = 2 * length) {
+	const size_t HALF = arr.size() / 2;
+	for (std::size_t length = 1; length <= HALF; length = 2 * length) {
 
-//Merge the current runs
-		normal_merge(arr, (*help_array), length);
-
-//copy the array back
-		arr = (*help_array);
+		//Merge the current runs
+		bitonic_merge(arr, (*help_array), length);
 	}
+
 }
 
--- src/ExcerciseD_Test.cpp
+++ src/ExcerciseD_Test.cpp
@@ -21,22 +21,15 @@ class ExcerciseD: public ::testing::Test {
 
 TEST(BottomUpMergeSort, double) {
 	//because unique ptr may be buggy in reclaiming ressouces
-	std::shared_ptr<std::array<double, 10>> array(new std::array<double, 10>);
+	std::shared_ptr<std::array<double, 8>> array(new std::array<double, 8>);
 
 	genrate_array_desc(*array);
-//	for (std::size_t i = 0; i < array->size(); i++) {
-//
-//		std::cout << (*array)[i] << "," << std::endl;
-//
-//	}
 
 	bottomUpMergeSort(*array);
+
 	double compare = 1.0;
 	for (std::size_t i = 0; i < array->size(); i++) {
-
-//		std::cout << (*array)[i] << "," << std::endl;
 		EXPECT_EQ(compare , (*array)[i]);
-
 		compare++;
 	}
 
