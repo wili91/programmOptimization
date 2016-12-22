888ab8a9a7f8af22108fb7c65c12eca6b0e03ea6
Lisa Winter <ljb.win10@gmail.com>
2016-12-22 11:29:44 +0100

    Added Hybrid Sort tests

--- src/ExcerciseE_Test.cpp
+++ src/ExcerciseE_Test.cpp
@@ -13,6 +13,9 @@ class ExcerciseE: public ::testing::Test {
 
 };
 
+
+
+
 TEST(QuickSort_3Partition, int) {
 	//because unique ptr may be buggy in reclaiming ressouces
 	std::array<int, 8> array ={9,7,6,6,7,1,4,5};
@@ -41,4 +44,48 @@ TEST(QuickSort_3Partition, int_desc) {
 		EXPECT_EQ(compare , (*array)[i]);
 		compare++;
 	}
+}
+
+TEST(Hyprid_QuickSort_3Partition, int) {
+	//because unique ptr may be buggy in reclaiming ressouces
+	std::array<int, 8> array ={9,7,6,6,7,1,4,5};
+
+	hybrid_partition_sort(array, 0, array.size());
+	for (std::size_t i = 0; i < array.size(); i++) {
+
+	std::cout<< array[i]<<",";
+
+	}
+	std::cout<<std::endl;
+
+	EXPECT_EQ(1 , array[0]);
+	EXPECT_EQ(4 , array[1]);
+	EXPECT_EQ(5 , array[2]);
+	EXPECT_EQ(6 , array[3]);
+	EXPECT_EQ(6 , array[4]);
+	EXPECT_EQ(7 , array[5]);
+	EXPECT_EQ(7 , array[6]);
+	EXPECT_EQ(9 , array[7]);
+
+	std::array<int, 10> array2 ={9,7,6,6,7,1,4,4,2,3};
+
+	hybrid_partition_sort(array2, 0, array2.size());
+
+	for (std::size_t i = 0; i < array2.size(); i++) {
+
+	std::cout<< array2[i]<<",";
+
+	}
+	std::cout<<std::endl;
+
+	EXPECT_EQ(1 , array2[0]);
+	EXPECT_EQ(2 , array2[1]);
+	EXPECT_EQ(3 , array2[2]);
+	EXPECT_EQ(4 , array2[3]);
+	EXPECT_EQ(4 , array2[4]);
+	EXPECT_EQ(6 , array2[5]);
+	EXPECT_EQ(6 , array2[6]);
+	EXPECT_EQ(7 , array2[7]);
+	EXPECT_EQ(7 , array2[8]);
+	EXPECT_EQ(9 , array2[9]);
 }
