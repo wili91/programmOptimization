2daa1894501598ca06ad40c52d7dd0b08325c228
Lisa Winter <ljb.win10@gmail.com>
2017-01-02 15:35:17 +0100

    fixed Mergesort and added Testcases

--- include/excerxiseD.h
+++ include/excerxiseD.h
@@ -44,56 +44,40 @@ void bitonic_merge(std::array<T, N>& arr1, std::array<T, N>& help,
 	//  ^            ^          ^
 	//  left        mid        right
 
-	std::size_t left 	= merge_index;
-	std::size_t mid 	= std::min(end, merge_index + length);
-	std::size_t right 	= (std::min(end, merge_index + 2 * length));
-
-	std::cout<< "merge_index : " << merge_index<< " end: " << end <<  std::endl <<  std::endl;
-
-	std::cout<< "left : " << left << " mid: " << mid << " right: "<< right <<  std::endl;
+	std::size_t left = merge_index;
+	std::size_t mid = std::min(end, merge_index + length);
+	std::size_t right = (std::min(end, merge_index + 2 * length));
 
 	//copy right side( left -> mid ) to help
 	for (std::size_t itr = left; itr < mid; itr++) {
 		help[itr] = arr1[itr];
-		std::cout << help[itr] << ",";
+
 	}
 
 	//copy left  side ( mid -> right) to help
 	for (std::size_t itr = mid, r_itr = right; mid < r_itr; itr++) {
 		help[itr] = arr1[--r_itr];
-		std::cout << help[itr] << ",";
+
 	}
 
-	std::cout << "||"<<std::endl;
-
-	std::size_t left_Runitr 	= left;
-	std::size_t right_Runitr 	= right;
-	std::size_t write_id		= merge_index;
+	std::size_t left_Runitr = left;
+	std::size_t right_Runitr = right;
+	std::size_t write_id = merge_index;
 
 	while (left_Runitr < right_Runitr) {
 
 		if (help[left_Runitr] < help[--right_Runitr]) {
-			std::cout << "left is smaller. left ptr: "<< left_Runitr <<std::endl;
 			arr1[write_id] = help[left_Runitr];
 			left_Runitr++;
 			right_Runitr++;
 		} else {
-			std::cout << "right is smaller. right ptr: "<< right_Runitr <<std::endl;
 			arr1[write_id] = help[right_Runitr];
 		}
 		//inkrement write index
 		write_id++;
-		std::cout << "write index: "<< write_id <<" left ptr:" << left_Runitr <<" right ptr:" << right_Runitr <<std::endl;
 	}
 
-	for (std::size_t i = left; i < right; i++) {
-
-		std::cout << arr1[i] << ",";
-
-	}
-	std::cout << "~~"<<std::endl;
 }
-
 
 template<std::size_t N, typename T>
 void bottomUpMergeSort(std::array<T, N>& arr) {
@@ -102,10 +86,8 @@ void bottomUpMergeSort(std::array<T, N>& arr) {
 	std::shared_ptr<std::array<T, N> > help_array(new std::array<T, N>);
 
 	for (std::size_t length = 1; length < arr.size(); length = 2 * length) {
-		std::cout<< "################### run length: " << length<<" ##################" <<  std::endl;
 		for (std::size_t merge_index = 0; merge_index < arr.size();
 				merge_index += 2 * length) {
-			std::cout<< "------------- merge index: " << merge_index <<" -------------" <<  std::endl;
 			//Merge the current runs
 			bitonic_merge(arr, (*help_array), merge_index, length, arr.size());
 		}
--- src/ExcerciseD_Test.cpp
+++ src/ExcerciseD_Test.cpp
@@ -19,130 +19,193 @@ class ExcerciseD: public ::testing::Test {
 
 };
 
+TEST(MergeTest, specialCases) {
+	std::shared_ptr<std::array<int, 0>> tmp0(new std::array<int, 0>());
+	std::shared_ptr<std::array<int, 1>> tmp1(new std::array<int, 1>());
+	std::shared_ptr<std::array<int, 2>> tmp2(new std::array<int, 2>());
+	std::shared_ptr<std::array<int, 3>> tmp3(new std::array<int, 3>());
 
-TEST(MergeTest, specialCase) {
-    std::shared_ptr<std::array<int, 0>> tmp0(new std::array<int, 0>());
-    std::shared_ptr<std::array<int, 1>> tmp1(new std::array<int, 1>());
-    std::shared_ptr<std::array<int, 2>> tmp2(new std::array<int, 2>());
-    std::shared_ptr<std::array<int, 3>> tmp3(new std::array<int, 3>());
+	std::shared_ptr<std::array<int, 0>> array_empty(new std::array<int, 0>);
+	bitonic_merge(*array_empty, *tmp0, 0, 0, array_empty->size());
+	checkSorted(*array_empty);
 
-    std::shared_ptr<std::array<int, 0>> array_empty(new std::array<int, 0>);
-    bitonic_merge(*array_empty, *tmp0, 0, 0, array_empty->size());
-    checkSorted(*array_empty);
+	std::shared_ptr<std::array<int, 1>> array_one(new std::array<int, 1> { 8 });
+	bitonic_merge(*array_one, *tmp1, 0, 1, array_one->size());
+	checkSorted(*array_one);
 
-    std::shared_ptr<std::array<int, 1>> array_one(new std::array<int, 1>{8});
-    bitonic_merge(*array_one, *tmp1, 0, 1, array_one->size());
-    checkSorted(*array_one);
+	std::shared_ptr<std::array<int, 2>> array_two0(
+			new std::array<int, 2> { 4, 5 });
+	bitonic_merge(*array_two0, *tmp2, 0, 2, array_two0->size());
+	checkSorted(*array_two0);
 
-    std::shared_ptr<std::array<int, 2>> array_two0(new std::array<int, 2>{4,5});
-    bitonic_merge(*array_two0, *tmp2, 0, 2, array_two0->size());
-    checkSorted(*array_two0);
+	std::shared_ptr<std::array<int, 2>> array_two1(
+			new std::array<int, 2> { 9, 4 });
+	bitonic_merge(*array_two1, *tmp2, 0, 2, array_two1->size());
+	checkSorted(*array_two1);
 
-    std::shared_ptr<std::array<int, 2>> array_two1(new std::array<int, 2>{9,4});
-    bitonic_merge(*array_two1, *tmp2, 0, 2, array_two1->size());
-    checkSorted(*array_two1);
+	std::shared_ptr<std::array<int, 3>> array_three(new std::array<int, 3> { 9,
+			4, 6 });
+	//first run
+	bitonic_merge(*array_three, *tmp3, 0, 1, array_three->size());
+	bitonic_merge(*array_three, *tmp3, 2, 1, array_three->size());
+	//second run
+	bitonic_merge(*array_three, *tmp3, 0, 2, array_three->size());
+	checkSorted(*array_three);
 
-    std::shared_ptr<std::array<int, 3>> array_three(new std::array<int, 3>{9,4,6});
-    //first run
-    bitonic_merge(*array_three, *tmp3, 0, 1, array_three->size());
-    bitonic_merge(*array_three, *tmp3, 2, 1, array_three->size());
-    //second run
-    bitonic_merge(*array_three, *tmp3, 0, 2, array_three->size());
-    checkSorted(*array_three);
-
-    std::shared_ptr<std::array<int, 3>> array_three1(new std::array<int, 3>{5,7,10});
-    //first run
-    bitonic_merge(*array_three1, *tmp3, 0, 1, array_three1->size());
-    bitonic_merge(*array_three1, *tmp3, 2, 1, array_three1->size());
-    //second run
-    bitonic_merge(*array_three1, *tmp3, 0, 2, array_three1->size());
-    checkSorted(*array_three1);
+	std::shared_ptr<std::array<int, 3>> array_three1(new std::array<int, 3> { 5,
+			7, 10 });
+	//first run
+	bitonic_merge(*array_three1, *tmp3, 0, 1, array_three1->size());
+	bitonic_merge(*array_three1, *tmp3, 2, 1, array_three1->size());
+	//second run
+	bitonic_merge(*array_three1, *tmp3, 0, 2, array_three1->size());
+	checkSorted(*array_three1);
 }
 
-//TEST(BottomUpMergeSort, integer) {
-//	std::array<int, 8> array2 = { 9, 7, 6, 6, 7, 1, 4, 4 };
-//
-//	for (std::size_t i = 0; i < array2.size(); i++) {
-//
-//		std::cout << array2[i] << ",";
-//
-//	}
-//	std::cout << std::endl;
-//
-//
-//	bottomUpMergeSort(array2);
-//
-//	for (std::size_t i = 0; i < array2.size(); i++) {
-//
-//		std::cout << array2[i] << ",";
-//
-//	}
-//	std::cout << std::endl;
-//
-//	EXPECT_EQ(1, array2[0]);
-//	EXPECT_EQ(4, array2[1]);
-//	EXPECT_EQ(4, array2[2]);
-//	EXPECT_EQ(6, array2[3]);
-//	EXPECT_EQ(6, array2[4]);
-//	EXPECT_EQ(7, array2[5]);
-//	EXPECT_EQ(7, array2[6]);
-//	EXPECT_EQ(9, array2[7]);
-//}
-//
-//TEST(BottomUpMergeSort, double) {
-//	//because unique ptr may be buggy in reclaiming ressouces
-//	std::shared_ptr<std::array<double, 8>> array(new std::array<double, 8>);
-//
-//	genrate_array_desc(*array);
-//
-//	bottomUpMergeSort(*array);
-//	for (std::size_t i = 0; i < array->size(); i++) {
-//
-//		std::cout << (*array)[i] << ",";
-//
-//	}
-//	std::cout << std::endl;
-//
-//	double compare = 1.0;
-//	for (std::size_t i = 0; i < array->size(); i++) {
-//		EXPECT_EQ(compare, (*array)[i]);
-//		compare++;
-//	}
-//}
-//
-//TEST(BottomUpMergeSort, double_no_power2) {
-//
-//	std::array<int, 10> array2 = { 9, 7, 6, 6, 7, 1, 4, 4, 2, 1 };
-//
-//
-//	for (std::size_t i = 0; i < array2.size(); i++) {
-//
-//	std::cout<< array2[i]<<",";
-//
-//	}
-//
-//	std::cout<<std::endl;
-//	bottomUpMergeSort(array2);
-//
-//	for (std::size_t i = 0; i < array2.size(); i++) {
-//
-//	std::cout<< array2[i]<<",";
-//
-//	}
-//	std::cout<<std::endl;
-//
-//	EXPECT_EQ(1, array2[0]);
-//	EXPECT_EQ(1, array2[1]);
-//	EXPECT_EQ(2, array2[2]);
-//	EXPECT_EQ(4, array2[3]);
-//	EXPECT_EQ(4, array2[4]);
-//	EXPECT_EQ(6, array2[5]);
-//	EXPECT_EQ(6, array2[6]);
-//	EXPECT_EQ(7, array2[7]);
-//	EXPECT_EQ(7, array2[8]);
-//	EXPECT_EQ(9, array2[9]);
-//
-//}
+TEST(MergeTest, oddCases) {
+	std::shared_ptr<std::array<int, 10>> tmp0(new std::array<int, 10>());
 
+	std::shared_ptr<std::array<int, 10>> array(new std::array<int, 10> { 1, 2,
+			3, 4, 5, 6, 7, 8, 1, 2 });
+	bitonic_merge(*array, *tmp0, 0, 8, array->size());
+	checkSorted(*array);
+}
 
+TEST(MergeTest, String) {
+	std::shared_ptr<std::array<std::string, 0>> tmp0(
+			new std::array<std::string, 0>());
+	std::shared_ptr<std::array<std::string, 1>> tmp1(
+			new std::array<std::string, 1>());
+	std::shared_ptr<std::array<std::string, 2>> tmp2(
+			new std::array<std::string, 2>());
+	std::shared_ptr<std::array<std::string, 3>> tmp3(
+			new std::array<std::string, 3>());
 
+	std::shared_ptr<std::array<std::string, 0>> array_empty(
+			new std::array<std::string, 0>);
+	bitonic_merge(*array_empty, *tmp0, 0, 0, array_empty->size());
+	checkSorted(*array_empty);
+
+	std::shared_ptr<std::array<std::string, 1>> array_one(
+			new std::array<std::string, 1> { { "aaa" } });
+	bitonic_merge(*array_one, *tmp1, 0, 1, array_one->size());
+	checkSorted(*array_one);
+
+	std::shared_ptr<std::array<std::string, 1>> array_empty_string(
+			new std::array<std::string, 1> { { "" } });
+	bitonic_merge(*array_empty_string, *tmp1, 0, 1, array_empty_string->size());
+	checkSorted(*array_empty_string);
+
+	std::shared_ptr<std::array<std::string, 2>> array_two0(
+			new std::array<std::string, 2> { { "", "a" } });
+	bitonic_merge(*array_two0, *tmp2, 0, 2, array_two0->size());
+	checkSorted(*array_two0);
+
+	std::shared_ptr<std::array<std::string, 2>> array_two1(
+			new std::array<std::string, 2> { { "b", "a" } });
+	bitonic_merge(*array_two1, *tmp2, 0, 2, array_two1->size());
+	checkSorted(*array_two1);
+
+	std::shared_ptr<std::array<std::string, 2>> array_two3(
+			new std::array<std::string, 2> { { "abb", "ax3" } });
+	bitonic_merge(*array_two3, *tmp2, 0, 2, array_two3->size());
+	checkSorted(*array_two3);
+
+	std::shared_ptr<std::array<std::string, 3>> array_three1(
+			new std::array<std::string, 3> { { "abab", "ax3", "42" } });
+	//first run
+	bitonic_merge(*array_three1, *tmp3, 0, 1, array_three1->size());
+	bitonic_merge(*array_three1, *tmp3, 2, 1, array_three1->size());
+	//second run
+	bitonic_merge(*array_three1, *tmp3, 0, 2, array_three1->size());
+	checkSorted(*array_three1);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+TEST(BottomUpMergeSort, integer) {
+	std::array<int, 0> array0;
+	bottomUpMergeSort(array0);
+	checkSorted(array0);
+
+	std::array<int, 1> array = { 1 };
+	bottomUpMergeSort(array);
+	checkSorted(array);
+
+	std::array<int, 10> array1 = { 9, 7, 6, 6, 7, 1, 4, 4, 2, 1 };
+	bottomUpMergeSort(array1);
+	checkSorted(array1);
+
+	std::array<int, 8> array2 = { 9, 7, 6, 6, 7, 1, 4, 4 };
+	bottomUpMergeSort(array2);
+	checkSorted(array2);
+}
+
+TEST(BottomUpMergeSort, double) {
+	//because unique ptr may be buggy in reclaiming ressouces
+	std::shared_ptr<std::array<double, 8>> array(new std::array<double, 8>);
+
+	genrate_array_desc(*array);
+
+	bottomUpMergeSort(*array);
+	checkSorted(*array);
+}
+
+TEST(BottomUpMergeSort, double_no_power2) {
+
+	std::array<double, 0> array0;
+	bottomUpMergeSort(array0);
+	checkSorted(array0);
+
+	std::array<double, 1> array = { 1.9 };
+	bottomUpMergeSort(array);
+	checkSorted(array);
+
+	std::array<double, 8> array1 = { 9.5, 7.1, 6.9, 6.4, 7.9, 1., 4., 4. };
+	bottomUpMergeSort(array1);
+	checkSorted(array1);
+	std::array<double, 10> array2 = { 9.5, 7.1, 6.9, 6.4, 7.9, 1., 4., 4., 2.6,
+			1.1 };
+	bottomUpMergeSort(array2);
+	checkSorted(array2);
+
+}
+
+TEST(BottomUpMergeSort, String) {
+
+	std::shared_ptr<std::array<std::string, 1>> arr(
+			new std::array<std::string, 1> { { "aaa" } });
+	bottomUpMergeSort(*arr);
+	checkSorted(*arr);
+
+	std::shared_ptr<std::array<std::string, 1>> arr1(
+			new std::array<std::string, 1> { { "" } });
+	bottomUpMergeSort(*arr1);
+	checkSorted(*arr1);
+
+	std::shared_ptr<std::array<std::string, 2>> arr2(
+			new std::array<std::string, 2> { { "", "a" } });
+	bottomUpMergeSort(*arr2);
+	checkSorted(*arr2);
+
+	std::shared_ptr<std::array<std::string, 7>> arr3(
+			new std::array<std::string, 7> { { "Äpfel ", "und ", " Birnen ",
+					"und", " Bananen ", "sind ", "gesund." } });
+	bottomUpMergeSort(*arr3);
+	checkSorted(*arr3);
+
+	std::shared_ptr<std::array<std::string, 7>> arr4(
+			new std::array<std::string, 7> { { "aaa ", "bb ", " a ", "", "cc",
+					"abc", "xyz" } });
+	bottomUpMergeSort(*arr4);
+	checkSorted(*arr4);
+
+	std::shared_ptr<std::array<std::string, 3>> arr5(
+			new std::array<std::string, 3> { { "Hallo ", "Welt ", "!" } });
+	bottomUpMergeSort(*arr5);
+	checkSorted(*arr5);
+
+}
